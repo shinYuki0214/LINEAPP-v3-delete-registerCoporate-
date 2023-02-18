03d7fcf513cf9438b0eddf45485c9aeb4c8eb534
shinjiro <>
2023-02-18 21:02:57 +0900

    メッセージチェック

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -16,26 +16,19 @@ class LineMessengerController extends Controller
     {
         // LINEから送られた内容を$inputsに代入
         $inputs = $request->all();
-
         // そこからtypeをとりだし、$message_typeに代入
         $message_type = $inputs['events'][0]['type'];
-
         // メッセージが送られた場合、$message_typeは'message'となる。その場合処理実行。
         if ($message_type == 'message') {
-
             // replyTokenを取得
             $reply_token = $inputs['events'][0]['replyToken'];
-
             // LINEBOTSDKの設定
             $http_client = new CurlHTTPClient(config('services.line.channel_token'));
             $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
-
             // 送信するメッセージの設定
             $reply_message = 'メッセージありがとうございますアプリから';
-
             // ユーザーにメッセージを返す
             $reply = $bot->replyText($reply_token, $reply_message);
-
             return 'ok';
         }
     }
