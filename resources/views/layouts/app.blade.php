2a79aeb7214de3ddf24eaedf9e10697866f82a0c
shinjiro <>
2023-02-18 20:52:47 +0900

    line menuadd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,6 +48,10 @@
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('linelogin') }}">LINEログイン</a>
+                            </li>
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
