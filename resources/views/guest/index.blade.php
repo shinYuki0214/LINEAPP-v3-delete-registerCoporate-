d06c4ca6fc2c888811efb0d04a5cf3a6e24f0f2b
shinjiro <>
2023-02-22 17:45:02 +0900

    login page

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,32 +17,49 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
+
 <body>
-    
-  <div class="form-signin-area">
-      <main class="form-signin">
-        <form>
-          <img class="mb-4" src="" alt="" width="72" height="57">
-          <h1 class="h3 mb-3 fw-normal">ログイン</h1>
-          <div class="form-floating">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
-          </div>
-          <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
-          </div>
-          <div class="checkbox mb-3">
-            <label>
-              <input type="checkbox" value="remember-me"> Remember me
-            </label>
-          </div>
-          <button class="w-100 btn btn-lg btn-primary mb-3" type="submit">ログイン</button>
-        </form>
-        <a href="{{ route('linelogin') }}" class="w-100 btn btn-lg btn-success" type="submit">LINEでログイン</a>
-        <p class="mt-5 mb-3 text-muted">&copy; 2023iwata</p>
-      </main>
-  </div>
-    
+
+    <div class="form-signin-area">
+        <main class="form-signin">
+            <form method="POST" action="{{ route('login') }}">
+                <img class="mb-4" src="" alt="" width="72" height="57">
+                <h1 class="h3 mb-3 fw-normal">ログイン</h1>
+                <div class="form-floating">
+                    <input id="floatingInput" type="email" class="form-control @error('email') is-invalid @enderror"
+                        name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+
+                    <input id="floatingPassword" type="password"
+                        class="form-control @error('password') is-invalid @enderror" name="password" required
+                        autocomplete="current-password">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    <label for="floatingPassword">Password</label>
+                </div>
+                <div class="checkbox mb-3">
+                    <label>
+                        <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                            {{ old('remember') ? 'checked' : '' }}>Remember me
+                    </label>
+                </div>
+                <button class="w-100 btn btn-lg btn-primary mb-3" type="submit">ログイン</button>
+            </form>
+            <a href="{{ route('linelogin') }}" class="w-100 btn btn-lg btn-success" type="submit">LINEでログイン</a>
+            <p class="mt-5 mb-3 text-muted">&copy; 2023iwata</p>
+        </main>
+    </div>
+
 </body>
+
 </html>
