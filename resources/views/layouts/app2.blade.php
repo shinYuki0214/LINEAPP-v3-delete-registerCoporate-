ee5b282184ddab268857e47ef50801446e24dd82
shinjiro <>
2023-02-24 12:51:16 +0900

    menu delete

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -59,8 +59,11 @@
             </div>
         </div>
     </header>
+
     <div class="container-fluid">
         <div class="row">
+            
+            @can('manager-higher')
             <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
                 <div class="position-sticky pt-3">
                     <ul class="nav flex-column">
@@ -101,7 +104,7 @@
 
                 </div>
             </nav>
-
+            @endcan
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div
                     class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
