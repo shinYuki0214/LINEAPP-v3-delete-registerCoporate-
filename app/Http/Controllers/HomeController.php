9dac70ca5063df73334cb618d08f0c32496e93e2
shinjiro <>
2023-02-25 09:06:23 +0900

    line to form

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+        if(is_null($user->name)){
+            return view('register.index');
+        }else{
+            return view('home');
+        }
     }
 }
