29d21d83462a6e08939d0f970adc642ba8ed8f58
shinYuki0214 <sin2022web@gmail.com>
2023-03-22 14:50:38 +0900

    non scalable

--- resources/views/layouts/app3.blade.php
+++ resources/views/layouts/app3.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -37,7 +37,7 @@
 <body>
 
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="{{route('manager.index')}}">発注システム</a>
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="{{ route('manager.index') }}">発注システム</a>
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
             data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
             aria-label="Toggle navigation">
