25302ee6bc7fa7eee68e9d622f03357b5b9964bb
shinYuki0214 <sin2022web@gmail.com>
2023-03-24 18:19:30 +0900

    news add

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -53,20 +53,6 @@
             </button>
         @endcan
         {{-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> --}}
-        <div class="navbar-nav">
-            <div class="nav-item text-nowrap">
-                {{-- <a class="" href="#">Sign out</a> --}}
-                <a class="nav-link px-3" href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
-                     document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                </form>
-            </div>
-        </div>
     </header>
 
     <div class="container-fluid">
